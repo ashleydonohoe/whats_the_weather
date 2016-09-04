1352b288528d811c8fe7fa39b567514c2e0de9da
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-09-03 22:19:42 -0400

    Added wunderground attribution

---
+++ What's The Weather/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  What's The Weather
+//
+//  Created by Gabriele on 9/3/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let ApiKey = "b41bb7731ab9b445"
+}
